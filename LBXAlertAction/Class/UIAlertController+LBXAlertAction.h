72484bbd2f5d17681fc2d45c82b7eb9f4a3d68fd
lbxia <lbxia20091227@foxmail.com>
2018-08-08 17:28:07 +0800

    兼容设备旋转

---
+++ LBXAlertAction/Class/UIAlertController+LBXAlertAction.h
@@ -0,0 +1,12 @@
+//
+//  UIAlertController+LBXAlertAction.h
+//  LBXAlertAction
+//
+//  Created by lbxia on 2018/8/8.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIAlertController (LBXAlertAction)
+//@property (nonatomic, weak) UIViewController *pvc;
+@end
